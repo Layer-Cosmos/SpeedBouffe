87c69cadb18c6b0e7aad2f64159a8cee9a112529
Layer-Etna <alary20@hotmail.fr>
2017-10-24 18:03:52 +0200

    ajout de plein de chose

--- html/script.js
+++ html/script.js
@@ -1,4 +1,32 @@
-var conn = new WebSocket('ws://localhost:8080');
+
+var conn = new ab.Session('ws://localhost:8080',
+    function() {
+        conn.subscribe('Mademoiselle', function(topic, data) {
+            var text = "";
+            var msg = data;
+            console.log(msg.title);
+            text = "Nouvelle commande de " + msg.Civilite + ". " + msg.Nom + " " + msg.Prenom;
+            var node = document.createElement("LI");                 // Create a <li> node
+            var textnode = document.createTextNode(text);         // Create a text node
+            node.appendChild(textnode);                              // Append the text to <li>
+            document.getElementById("ul").appendChild(node);  
+            // This is where you would add the new article to the DOM (beyond the scope of this tutorial)
+            console.log('New article published to category "' + topic + '" : ' + data.title);
+        });
+    },
+    function() {
+        console.warn('WebSocket connection closed');
+    },
+    {'skipSubprotocolCheck': true}
+);
+
+
+
+
+
+
+
+/*var conn = new WebSocket('ws://localhost:8080');
 conn.onopen = function(e) {
     console.log("Connection established!");
 };
@@ -12,4 +40,4 @@ conn.onmessage = function(e) {
     var textnode = document.createTextNode(text);         // Create a text node
     node.appendChild(textnode);                              // Append the text to <li>
     document.getElementById("ul").appendChild(node);  
-};
+};*/
---
+++ src/push-server.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Layer
+ * Date: 24/10/2017
+ * Time: 16:48
+ */
+    require dirname(__DIR__) . '/vendor/autoload.php';
+
+    $loop   = React\EventLoop\Factory::create();
+    $pusher = new MyApp\Serveur\Pusher();
+    //$pusher = new  MyApp\Serveur\Chat();
+
+    // Listen for the web server to make a ZeroMQ push after an ajax request
+    $context = new React\ZMQ\Context($loop);
+    $pull = $context->getSocket(ZMQ::SOCKET_PULL);
+    $pull->bind('tcp://127.0.0.1:5555'); // Binding to 127.0.0.1 means the only client that can connect is itself
+    $pull->on('message', array($pusher, 'onBlogEntry'));
+
+    // Set up our WebSocket server for clients wanting real-time updates
+    var_dump($pusher);
+    $webSock = new React\Socket\Server('0.0.0.0:8080', $loop); // Binding to 0.0.0.0 means remotes can connect
+    $webServer = new Ratchet\Server\IoServer(
+        new Ratchet\Http\HttpServer(
+            new Ratchet\WebSocket\WsServer(
+                new Ratchet\Wamp\WampServer(
+                    $pusher
+                )
+            )
+        ),
+        $webSock
+    );
+
+
+    /*$webSock = new React\Socket\Server('0.0.0.0:8080', $loop); // Binding to 0.0.0.0 means remotes can connect
+    $webServer = new Ratchet\Server\IoServer(
+        new Ratchet\Http\HttpServer(
+            new Ratchet\WebSocket\WsServer(
+                new MyApp\Serveur\Chat(
+                )
+            )
+        ),
+        $webSock
+    );*/
+
+
+    $loop->run();
